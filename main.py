593cbd49e7207c335f6a56e74e4c725a0e6ea90b
Kevin Tang <kvntng@gmail.com>
2019-08-03 12:51:13 -0400

    Bug fixes

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 # Kevin Tang
 # Started 7/10/2019
 
-from scrape import scrape_videos
+from scrape import scrape_video
 
 
 def main():
 
 	# WORKS
-	scrape_videos('https://www.reddit.com/r/aww/comments/c5xurx/my_dad_sent_me_this_and_i_have_a_new_favorite/')
-	# scrape_videos('https://www.reddit.com/r/aww/comments/b0fi8e/head_scritchesinstant_golden_smiles/')
-	# scrape_videos('https://www.reddit.com/r/aww/comments/9r9gfb/kids_sing_happy_birthday_with_sign_language_for/')
+	scrape_video('https://www.reddit.com/r/aww/comments/c5xurx/my_dad_sent_me_this_and_i_have_a_new_favorite/')
+	# scrape_video('https://www.reddit.com/r/aww/comments/b0fi8e/head_scritchesinstant_golden_smiles/')
+	# scrape_video('https://www.reddit.com/r/aww/comments/9r9gfb/kids_sing_happy_birthday_with_sign_language_for/')
 
 
 if __name__ == "__main__":
